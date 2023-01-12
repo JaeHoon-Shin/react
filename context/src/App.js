4dbdc4ca545f1be5cb6f4dc01ef836ec5c279de4
JaeHoon-Shin <sinhoon0805@naver.com>
2023-01-12 14:17:40 +0900

    reaet수업 12.25~01.12

--- context/src/App.js
+++ context/src/App.js
@@ -1,12 +1,28 @@
+import { createContext, useContext } from 'react';
 import './App.css';
-
+const Mycontext = createContext('hello')
 function App() {
+  const num = 100;
+  /* const fn=()=>{return 1000} */
   return (
     <div className="App">
-    </div>
+      <Mycontext.Provider value={num}>
+        
+        <List />
+        
+      </Mycontext.Provider>
+      </div>
   );
 }
-function List(){}
+function List(){
+  const value = useContext(Mycontext);
+  return(
+    <>
+      List{value}<br/>
+      
+    </>
+  )
+}
 function Item(){}
 function Insert(){}
 export default App;
